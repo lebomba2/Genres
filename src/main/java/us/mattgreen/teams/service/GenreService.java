58c0f9a357c3c41a8bef612b368fdadd63cdfe68
lebomba2 <lebomba2@gmail.com>
2018-12-07 11:02:12 -0600

    Added logging and error html.

--- src/main/java/us/mattgreen/teams/service/GenreService.java
+++ src/main/java/us/mattgreen/teams/service/GenreService.java
@@ -1,5 +1,7 @@
 package us.mattgreen.teams.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 import us.mattgreen.teams.controllers.Genre;
 
@@ -10,6 +12,8 @@ import java.util.List;
 // Service annotation tells spring that this class is a service
 @Service
 public class GenreService {
+    private static Log log = LogFactory.getLog(GenreService.class);
+
     private List<Genre> genres = new ArrayList<>(Arrays.asList(
             new Genre("Action", "5-Awesome"),
             new Genre("Romantic Comedy", "2-lame"),
@@ -32,24 +36,32 @@ public class GenreService {
         // t.getGenreName() gets the name, .contains(id) checks if genreName contains "id"
         // .findFirst() returns the first match from contains
         // .get() gets the selected object
+        log.debug("Getting genre by id: \"" + id + "\"");
         return genres.stream().filter(t -> t.getGenreName().contains(id)).findFirst().get();
     }
 
     public void addGenre(Genre genre) {
+        log.debug("Adding genre: \"" + genre + "\"");
         genres.add(genre);
+        log.trace("Genre \"" + genre + "\" added successfully.");
     }
 
     public void updateGenre(Genre genre, String id) {
+        log.debug("Searching for genre by ID: \"" + id + "\"");
         for(int i = 0; i < genres.size() ;i++) {
             Genre g = genres.get(i);
             if (g.getGenreName().equals(id)) {
+                log.debug("genre \"" +g.getGenreName() + "\" found. Updating to \"" + genre + "\"");
                 genres.set(i, genre);
+                log.trace("Genre with id \"" + id + "\" updated successfully");
                 return;
             }
         }
     }
 
     public void deleteGenre(String id) {
+        log.debug("Deleting genre id " + id + "\" ...");
         genres.removeIf(g -> g.getGenreName().equals(id));
+        log.debug(id + " deleted successfully.");
     }
 }
